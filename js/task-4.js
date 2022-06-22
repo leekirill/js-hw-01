988e8c65b62d9f60325b09beee9896f1f8ef7b7f
Kirill Lee <cyril.lee1997@gmail.com>
2022-06-22 23:14:51 +0300

    task-4 — done

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,24 @@
+const counterValue = {
+    value: 0,
 
+    increment() {
+        return (this.value += 1);
+    },
+    decrement() {
+        return (this.value -= 1);
+    },
+};
+
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+const value = document.querySelector('span#value');
+
+incrementBtn.addEventListener(
+    'click',
+    handler => (value.textContent = counterValue.increment()),
+);
+
+decrementBtn.addEventListener(
+    'click',
+    handler => (value.textContent = counterValue.decrement()),
+);
