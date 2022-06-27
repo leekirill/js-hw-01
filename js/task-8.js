5cf8ab0c916382e3671f6406084d491fc5f5f172
Kirill Lee <cyril.lee1997@gmail.com>
2022-06-27 12:42:51 +0300

    task-8 — done

---
+++ js/task-8.js
@@ -0,0 +1,98 @@
+var colorArray = [
+    '#FF6633',
+    '#FFB399',
+    '#FF33FF',
+    '#FFFF99',
+    '#00B3E6',
+    '#E6B333',
+    '#3366E6',
+    '#999966',
+    '#99FF99',
+    '#B34D4D',
+    '#80B300',
+    '#809900',
+    '#E6B3B3',
+    '#6680B3',
+    '#66991A',
+    '#FF99E6',
+    '#CCFF1A',
+    '#FF1A66',
+    '#E6331A',
+    '#33FFCC',
+    '#66994D',
+    '#B366CC',
+    '#4D8000',
+    '#B33300',
+    '#CC80CC',
+    '#66664D',
+    '#991AFF',
+    '#E666FF',
+    '#4DB3FF',
+    '#1AB399',
+    '#E666B3',
+    '#33991A',
+    '#CC9999',
+    '#B3B31A',
+    '#00E680',
+    '#4D8066',
+    '#809980',
+    '#E6FF80',
+    '#1AFF33',
+    '#999933',
+    '#FF3380',
+    '#CCCC00',
+    '#66E64D',
+    '#4D80CC',
+    '#9900B3',
+    '#E64D66',
+    '#4DB380',
+    '#FF4D4D',
+    '#99E6E6',
+    '#6666FF',
+];
+
+const inputEl = document.querySelector('input');
+const createBtn = document.querySelector('button[data-action="render"]');
+const clearBtn = document.querySelector('button[data-action="destroy"]');
+const divBoxes = document.querySelector('div#boxes');
+
+function createBoxes(amount) {
+    const divArray = [];
+    for (let i = 0; i < amount; i++) {
+        divArray.push(i + 1);
+    }
+
+    divBoxes.insertAdjacentHTML(
+        'afterbegin',
+        divArray.map(
+            e =>
+                `<div style='width:${e * 10 + 20}px; height:${
+                    e * 10 + 20
+                }px; background-color:${
+                    colorArray[
+                        Math.floor(Math.random() * (colorArray.length - 1))
+                    ]
+                }'></div>`,
+        ),
+    );
+}
+
+function destroyBoxes() {
+    divBoxes.innerHTML = '';
+    inputEl.value = 0;
+}
+
+createBtn.addEventListener('click', handler => {
+    createBoxes(Number(inputEl.value));
+}); // по клику на кнопку Создать
+
+inputEl.addEventListener('keypress', handler => {
+    if (handler.key === 'Enter') createBoxes(Number(inputEl.value));
+}); // по Enter
+
+clearBtn.addEventListener('click', handler => {
+    destroyBoxes();
+});
+
+// сделать массив цветов экспортом
+// мб пофиксить повторное добавление div, надо чтоб дивы обновлялись
