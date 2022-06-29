bc093ed77cd6a798e2d7a9e652d6522650a89078
Kirill Lee <cyril.lee1997@gmail.com>
2022-06-29 10:37:46 +0300

    task 7 - done

--- js/task-7.js
+++ js/task-7.js
@@ -3,8 +3,8 @@ const refs = {
     textEl: document.querySelector('span#text'),
 };
 
-refs.rangeEl.addEventListener('input', fontSizeChanger);
-
-function fontSizeChanger() {
+const fontSizeChanger = function () {
     refs.textEl.setAttribute('style', `font-size: ${refs.rangeEl.value}px`);
-}
+};
+
+refs.rangeEl.addEventListener('input', fontSizeChanger);
