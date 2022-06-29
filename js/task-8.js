6c1b6272f414a670145a38bce3f4aa1c434307be
Kirill Lee <cyril.lee1997@gmail.com>
2022-06-29 10:47:28 +0300

    task-8 — done

--- js/task-8.js
+++ js/task-8.js
@@ -1,9 +1,11 @@
 import colorArray from './colorArray.js';
 
-const inputEl = document.querySelector('input');
-const createBtn = document.querySelector('button[data-action="render"]');
-const clearBtn = document.querySelector('button[data-action="destroy"]');
-const divBoxes = document.querySelector('div#boxes');
+const refs = {
+    inputEl: document.querySelector('input'),
+    createBtn: document.querySelector('button[data-action="render"]'),
+    clearBtn: document.querySelector('button[data-action="destroy"]'),
+    divBoxes: document.querySelector('div#boxes'),
+};
 
 function createBoxes(amount) {
     const divArray = [];
@@ -11,7 +13,7 @@ function createBoxes(amount) {
         divArray.push(i + 1);
     }
 
-    divBoxes.insertAdjacentHTML(
+    refs.divBoxes.insertAdjacentHTML(
         'afterbegin',
         divArray.map(
             e =>
@@ -26,27 +28,34 @@ function createBoxes(amount) {
     );
 }
 
-function destroyBoxes() {
-    divBoxes.innerHTML = '';
-    inputEl.value = 0;
+refs.createBtn.addEventListener('click', onClickCreateBoxes);
+refs.inputEl.addEventListener('keydown', onClickCreateBoxesByEnter);
+refs.inputEl.addEventListener('keydown', onClickDestroyAllBoxesByEsc);
+refs.clearBtn.addEventListener('click', onClickDestroyAllBoxes);
+
+function onClickCreateBoxes() {
+    refs.divBoxes.innerHTML = '';
+    createBoxes(Number(refs.inputEl.value));
+    refs.inputEl.style.marginBottom = '20px';
+    refs.divBoxes.style.display = 'flex';
+    refs.divBoxes.style.flexWrap = 'wrap';
 }
 
-createBtn.addEventListener('click', handler => {
-    divBoxes.innerHTML = '';
-    createBoxes(Number(inputEl.value));
-    inputEl.style.marginBottom = '20px';
-    divBoxes.style.display = 'flex';
-    divBoxes.style.flexWrap = 'wrap';
-}); // по клику на кнопку Создать
+function onClickCreateBoxesByEnter(event) {
+    refs.divBoxes.innerHTML = '';
+    if (event.key === 'Enter') createBoxes(Number(refs.inputEl.value));
+    refs.inputEl.style.marginBottom = '20px';
+    refs.divBoxes.style.display = 'flex';
+    refs.divBoxes.style.flexWrap = 'wrap';
+}
 
-inputEl.addEventListener('keypress', handler => {
-    divBoxes.innerHTML = '';
-    if (handler.key === 'Enter') createBoxes(Number(inputEl.value));
-    inputEl.style.marginBottom = '20px';
-    divBoxes.style.display = 'flex';
-    divBoxes.style.flexWrap = 'wrap';
-}); // по Enter
-
-clearBtn.addEventListener('click', handler => {
-    destroyBoxes();
-});
+function onClickDestroyAllBoxes() {
+    refs.divBoxes.innerHTML = '';
+    refs.inputEl.value = 0;
+}
+function onClickDestroyAllBoxesByEsc(event) {
+    if (event.key === 'Escape') {
+        refs.divBoxes.innerHTML = '';
+        refs.inputEl.value = 0;
+    }
+}
